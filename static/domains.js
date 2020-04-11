82fcdeb1fa91d2bd7181a8156ee809f454b1ebcc
Mexson Fernandes <robomex2020@gmail.com>
2020-04-11 14:21:47 +0530

    add domain json

---
+++ static/domains.js
@@ -0,0 +1,38 @@
+export default {
+    domains: {
+        social: [
+            {
+                "name": "",
+                "link": "",
+                "icon": ""
+            },
+            {
+                "name": "",
+                "link": "",
+                "icon": ""
+            },
+            {
+                "name": "",
+                "link": "",
+                "icon": ""
+            },
+            {
+                "name": "",
+                "link": "",
+                "icon": ""
+            }
+        ],
+        technology: [
+            {
+                "name": "",
+                "link": "",
+                "icon": ""
+            },
+            {
+                "name": "",
+                "link": "",
+                "icon": ""
+            }
+        ]
+    }
+}
