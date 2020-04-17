a30fa6088532c037be195d0847cd0b0974d4a9d5
Rahul Sharma <rahulsharma94595@gmail.com>
2020-04-17 13:01:49 +0530

    (Added) opacity onPageLoad + shimmer + checkAvailability + favicon icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,72 +1,67 @@
-
 export default {
-  mode: 'spa',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
-    '@nuxtjs/bulma',
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/proxy'
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-    proxy: true
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
-    postcss: {
-      preset: {
-        features: {
-          customProperties: false
-        }
-      }
+    mode: 'spa',
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+        ]
     },
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#fff' },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
+        '@nuxtjs/bulma',
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+        '@nuxtjs/proxy'
+    ],
+    /*
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    axios: {
+        proxy: true
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        postcss: {
+            preset: {
+                features: {
+                    customProperties: false
+                }
+            }
+        },
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
     }
-  }
 }
