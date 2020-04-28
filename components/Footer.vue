adc8fa004f806b97c5d3d769c1529b6abbc5a839
Mexson Fernandes (RoboMex) <robomex2020@gmail.com>
2020-04-29 02:39:35 +0530

    V0.0.1 first fix draft (#5)
    * completed api connection for proxy :fire:
    
    * V0.0.1 (#3)
    
    * add shimmer effect
    
    * (Added) shimmer effect
    
    * (Added) opacity onPageLoad + shimmer + checkAvailability + favicon icon
    
    * change ui
    
    * WebIDHunt (#4)
    
    * add shimmer effect
    
    * (Added) shimmer effect
    
    * add hit counter markdown
    
    * (Added) opacity onPageLoad + shimmer + checkAvailability + favicon icon
    
    * Blur effect
    
    Co-authored-by: Mexson Fernandes (RoboMex) <robomex2020@gmail.com>
    
    Co-authored-by: Rahul Sharma <44829457+rahulsharma991@users.noreply.github.com>

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,28 @@
 <template>
     <footer class="has-text-centered has-text-white">
-        <p>
+        <p class="center-align">
         Developed by RoboMx&trade; Team
         </p>
     </footer>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+
+* {
+ font-family: 'Roboto', sans-serif;
+}
 footer {
   height: 74px;
   background-color: #0a173e;
+}
+.center-align {
+  display: flex;
+  height: 74px;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: 1px;
+  color: white !important;
 }
 </style>
 
--- static/list.js
+++ static/list.js
@@ -1,76 +1,90 @@
 export default {
     domains: {
-        social: [
-            {
+        social: [{
                 "name": "Facebook",
                 "endpoint": "https://www.facebook.com/<username>",
-                "logo": "https://logo.clearbit.com/facebook.com"
+                "logo": "https://logo.clearbit.com/facebook.com",
+                "shimmer": "hide",
+                "available": 'none'
             },
             {
                 "name": "Twitter",
                 "endpoint": "https://www.twitter.com/<username>",
-                "logo": "https://logo.clearbit.com/twitter.com"
+                "logo": "https://logo.clearbit.com/twitter.com",
+                "shimmer": "hide",
+                "available": 'none'
             },
             {
                 "name": "Instagram",
                 "endpoint": "https://www.instagram.com/<username>",
-                "logo": "https://logo.clearbit.com/instagram.com"
+                "logo": "https://logo.clearbit.com/instagram.com",
+                "shimmer": "hide",
+                "available": 'none'
             },
             {
                 "name": "TikTok",
                 "endpoint": "https://www.tiktok.com/<username>",
-                "logo": "https://logo.clearbit.com/tiktok.com"
+                "logo": "https://logo.clearbit.com/tiktok.com",
+                "shimmer": "hide",
+                "available": 'none'
             },
-          {
-                "name": "Snapchat",
-                "endpoint": "https://www.snapchat.com/<username>",
-                "logo": "https://logo.clearbit.com/snapchat.com"
-            },
-          {
+            {
                 "name": "Linkedin",
                 "endpoint": "https://www.linkedin.com/<username>",
-                "logo": "https://logo.clearbit.com/linkedin.com"
+                "logo": "https://logo.clearbit.com/linkedin.com",
+                "shimmer": "hide",
+                "available": 'none'
             },
-          {
+            {
                 "name": "Telegram",
-                "endpoint": "https://telegram.org/<userName>",
-                "logo": "https://logo.clearbit.com/telegram.org"
+                "endpoint": "https://telegram.org/<username>",
+                "logo": "https://logo.clearbit.com/telegram.org",
             },
         ],
-        technology: [
-            {
+        technology: [{
                 "name": "Github",
                 "endpoint": "https://github.com/<username>",
-                "logo": "https://logo.clearbit.com/github.com"
-            }, 
+                "logo": "https://logo.clearbit.com/github.com",
+                "shimmer": "hide",
+                "available": 'none'
+            },
             {
                 "name": "GitLab",
                 "endpoint": "https://about.gitlab.com/<username>",
-                "logo": "https://logo.clearbit.com/gitlab.com"
+                "logo": "https://logo.clearbit.com/gitlab.com",
+                "shimmer": "hide",
+                "available": 'none'
             },
             {
                 "name": "GitKraken",
                 "endpoint": "https://www.gitkraken.com/<username>",
-                "logo": "https://logo.clearbit.com/gitkraken.com"
+                "logo": "https://logo.clearbit.com/gitkraken.com",
+                "shimmer": "hide",
+                "available": 'none'
             }
         ],
-        communication: [
-          {
-            "name": "Skype",
-            "endpoint": "https://www.skype.com/<username>",
-            "logo": "https://logo.clearbit.com/skyped.ru"
-          },
-           {
-            "name": "Slack",
-            "endpoint": "https://slack.com/<username>",
-            "logo": "https://logo.clearbit.com/slack.com"
-          },
-         
-           {
-            "name": "FlowDock",
-            "endpoint": "https://www.flowdock.com/<username>",
-            "logo": "https://logo.clearbit.com/flowdock.com"
-          },
+        communication: [{
+                "name": "Skype",
+                "endpoint": "https://www.skype.com/<username>",
+                "logo": "https://logo.clearbit.com/skyped.ru",
+                "shimmer": "hide",
+                "available": 'none'
+            },
+            {
+                "name": "Slack",
+                "endpoint": "https://slack.com/<username>",
+                "logo": "https://logo.clearbit.com/slack.com",
+                "shimmer": "hide",
+                "available": 'none'
+            },
+
+            {
+                "name": "FlowDock",
+                "endpoint": "https://www.flowdock.com/<username>",
+                "logo": "https://logo.clearbit.com/flowdock.com",
+                "shimmer": "hide",
+                "available": 'none'
+            },
         ]
     }
-}
+};
